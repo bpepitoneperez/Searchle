d49c40d1cde593a554031b165b1cb2c30e0d2920
Bryan Pepitone-Perez <brryann21@gmail.com>
2022-07-03 14:45:15 -0400

    main picture on home screen. commiting to work on laptop

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/wheres-waldo1.jpg" className='img-fluid shadow-4' alt="Where's Waldo Image" />
       </header>
     </div>
   );
