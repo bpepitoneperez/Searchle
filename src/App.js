4da8263cb25614cac7909980f1d7e00c31ad08ec
Bryan Pepitone-Perez <brryann21@gmail.com>
2022-07-03 20:13:02 -0400

    got coords working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getClickCoords = (e) => {
+    const x = Math.round(
+      (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
+    );
+    const y = Math.round(
+      (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
+    );
+    console.log(x, y)
+    return {x, y};
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/wheres-waldo1.jpg" className='img-fluid shadow-4' alt="Where's Waldo Image" />
+        <img src="/wheres-waldo1.jpg" onClick={getClickCoords} className='img-fluid shadow-4' alt="Where's Waldo1" />
       </header>
     </div>
   );
