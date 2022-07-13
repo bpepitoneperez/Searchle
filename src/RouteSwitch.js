735727da4d351938cea94df99408ebf9f452ea26
Bryan Pepitone-Perez <brryann21@gmail.com>
2022-07-13 19:56:10 -0400

    route path change

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./Components/App";
 import NotFound from "./Components/NotFound";
 import Layout from "./Components/Layout";
 
 const RouteSwitch = () => {
   return (
-    <HashRouter >
+    <BrowserRouter >
       <Routes>
-        <Route path="" element={<Layout />} >
+        <Route path="/Searchle" element={<Layout />} >
           <Route index element={<App />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
