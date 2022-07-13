b0dc5b3068c2468a747d2c46eb292b95cb59cd76
Bryan Pepitone-Perez <brryann21@gmail.com>
2022-07-13 19:32:34 -0400

    hashrouter

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./Components/App";
 import NotFound from "./Components/NotFound";
 import Layout from "./Components/Layout";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<App />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
