8ebec88a734f63fa133d528f0c699e7ed213d106
Bryan Pepitone-Perez <brryann21@gmail.com>
2022-07-13 19:20:53 -0400

    deploy steps

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-    baseURL: "http://localhost:3001/api"
-//   baseURL: "https://searchle.herokuapp.com/api",
+    baseURL: "https://searchle-api.herokuapp.com/api"
 });
 
 export default instance;
